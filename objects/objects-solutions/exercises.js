0c3e94203810ea19201858810c68fc307c1d5413
gabi <gabi.szilvasy@gmail.com>
2019-10-10 14:40:23 +0200

    objects exercises solutions

--- objects/objects-solutions/exercises.js
+++ objects/objects-solutions/exercises.js
@@ -2,8 +2,7 @@ const people = require('./people').people;
 
 //Write a function that return the number of people that have starships
 function cntHasStarShip() {
-    let filtered = people.filter(p => p.starships.length > 0);
-    return filtered.length;
+    return people.filter(p => p.starships.length > 0).length;
 }
 
 console.log(cntHasStarShip());
@@ -12,7 +11,6 @@ console.log(cntHasStarShip());
 function addVehiclesCount() {
     people.forEach(p => {
         p.vehiclesCount = p.vehicles.length;
-
     })
 }
 
@@ -21,7 +19,7 @@ console.log("Vehicles Cnt:");
 console.log("--------------");
 people.forEach(p => console.log(p.name, p.vehiclesCount));
 
-//Sort the people objects by the number of items in the array of films property
+//Sort the people object by the number of items in the array of films property
 function sortByFilmsCnt() {
     people.sort((p1, p2) => p1.films.length - p2.films.length);
 }
@@ -35,9 +33,9 @@ people.forEach(p => console.log(p.name, p.films.length));
 //Sort the people objects by the number of items in the starships array and within that by the number of items in the vehicles array
 function sortByStarsipsandVehicles() {
     people.sort((p1, p2) => {
-        let diff = p1.starships.lenght - p2.starships.length;
-        return diff == 0 ? p1.vehicles.length - p2.vehicles.length : diff;
-    })
+        let stord = p1.starships.length - p2.starships.length;
+        return stord == 0 ? p1.vehiclesCount - p2.vehiclesCount : stord;
+    });
 }
 console.log(' ');
 console.log("Sorted by starships, vehicles:");
@@ -47,7 +45,7 @@ people.forEach(p => console.log(p.name, p.starships.length, p.vehiclesCount));
 
 //Count the total number of vehicles
 function countVehicles() {
-
+    return people.reduce((sum, p) => p.vehiclesCount + sum, 0);
 }
 
 console.log(' ');
--- objects/objects-solutions/peopleapi.js
+++ objects/objects-solutions/peopleapi.js
@@ -1,6 +1,16 @@
-const people = require('./people.js').people;
+const people = require('./people').people;
 
-//create a module exporting the following functions:
+const propTypes = {
+    "name": "str",
+    "height": "num",
+    "mass": "num",
+    "hair_color": "str",
+    "skin_color": "str",
+    "eye_color": "str",
+    "birth_year": "str",
+    "gender": "str",
+}
+
 
 //write a function that returns a people object, which name property equals name
 //sample input: 'Luke Skywalket'
@@ -12,7 +22,10 @@ const people = require('./people.js').people;
 //          eye_color: 'blue', ... }
 //
 function byName(name) {
-
+    let obj = byProp('name', name);
+    if (obj.length > 0)
+        return obj[0];
+    return undefined;
 }
 
 //write a function that returns an array of people objects, which exe_color property equals color
@@ -25,7 +38,7 @@ function byName(name) {
 //          eye_color: 'blue', ... }, {...} ]
 //
 function byEyeColor(color) {
-
+    return byProp('eye_color', color);
 }
 
 //write a function that returns an array of people objects, which prop property equals value
@@ -38,9 +51,17 @@ function byEyeColor(color) {
 //          eye_color: 'blue', ... }, {...} ]
 //
 function byProp(prop, value) {
-
+    return people.filter(p => p[prop] == value);
 }
 
+function getPropType(propName) {
+    let prop = propTypes[propName];
+    return (prop || "str");
+}
+
+function getValue(obj, prop, type) {
+    return type == "num" ? parseInt(obj[prop]) : obj[prop];
+}
 
 
 //write a sort function that sorts people objects by the property specified in prop argument,
@@ -49,7 +70,8 @@ function byProp(prop, value) {
 //sample input: "mass", false
 //       outpur: people array sorted by the mass property desc
 function sort(prop, asc = true) {
-
+    const pType = getPropType(prop);
+    people.sort((p1, p2) => (getValue(p1, prop, pType) - getValue(p2, prop, pType)) * (asc ? 1 : -1));
 }
 
 
@@ -62,17 +84,21 @@ function sort(prop, asc = true) {
 //       output: [{ name: 'Beru Whitesun lars', mass: '75', eye_color: 'blue' },
 //                { name: 'Leia Organa', mass: '49', eye_color: 'brown' }...]
 function all(fields) {
-    if (fields && fields.length > 0)
-        return people.map(p => {
-            let o = {};
+    return people.map(p => {
+        if (fields && fields.length > 0) {
+            let mapped = {};
             fields.forEach(field => {
-                o[field] = p[field];
+                mapped[field] = p[field];
             });
-            return o;
-        });
-    return people.map(p => p);
+            return mapped;
+        }
+        return p;
+    });
 }
 
 module.exports = {
-    every: all
-}
+    byName: byName,
+    byEyeColor: byEyeColor,
+    sort: sort,
+    all: all
+};
