adc7d0468820aea9d60d6c15b16dc0a6dd91a58e
gabi <gabi.szilvasy@gmail.com>
2019-11-14 09:21:09 +0100

    exercise files

---
+++ os-data-server/tcp-server/service/os-data.service.js
@@ -0,0 +1,33 @@
+const os = require('os');
+
+function getOSData() {
+
+    return {
+        user: 'User',
+        platform: os.platform(),
+        os: `${os.type()} ${os.release}`,
+        arch: os.arch(),
+        cpus: os.cpus().length,
+        mem: {
+            free: os.freemem() / 1024 / 1024 / 1024,
+            total: os.totalmem() / 1024 / 1024 / 1024
+        },
+        network: {
+            ethernet: getIpv4(os.networkInterfaces()['Ethernet']),
+            wireless: getIpv4(os.networkInterfaces()['Wi-Fi'])
+        }
+    }
+}
+
+function getIpv4(interfaces) {
+    if (interfaces) {
+        const int = interfaces.filter(intf => intf.family == 'IPv4').map(intf => intf.address);
+        if (int.length)
+            return int[0];
+    }
+    return '';
+}
+
+module.exports = {
+    getOSData: getOSData
+}
